6a0944c0be4e0c4094e91c7688e1766e164dbe9f
Rob Frankel <rob@robFX.com>
2020-12-04 15:12:24 -0500

    Part 2

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 
 namespace Day4
@@ -21,19 +22,17 @@ namespace Day4
             var count = 0;
             foreach (var passport in passports)
             {
-                var isValid = true;
-
                 string[] p = passport.Split(new[] { " ", "\n" }, StringSplitOptions.RemoveEmptyEntries);
-                Dictionary<string, string> d = p.Select(item => item.Split(':')).ToDictionary(i => i[0], i => i[1]);
+                Dictionary<string, string> fields = p.Select(item => item.Split(':')).ToDictionary(i => i[0], i => i[1]);
 
-                foreach (var field in requiredFields)
-                {
-                    if (!d.Keys.Contains(field))
-                    {
-                        isValid = false;
-                        break;
-                    }
-                }
+                var isValid = (fields.Keys.Contains("byr") && ValidateByr(fields["byr"])
+                            && fields.Keys.Contains("iyr") && ValidateIyr(fields["iyr"])
+                            && fields.Keys.Contains("eyr") && ValidateEyr(fields["eyr"])
+                            && fields.Keys.Contains("hgt") && ValidateHgt(fields["hgt"])
+                            && fields.Keys.Contains("hcl") && ValidateHcl(fields["hcl"])
+                            && fields.Keys.Contains("ecl") && ValidateEcl(fields["ecl"])
+                            && fields.Keys.Contains("pid") && ValidatePid(fields["pid"])
+                            ) ? true : false;
 
                 if (isValid)
                 {
@@ -42,6 +41,103 @@ namespace Day4
 
             }
             Console.WriteLine($"Valid: {count}");
+        }
+
+        private static bool ValidateByr(string input)
+        {
+            //four digits; at least 1920 and at most 2002.
+            var isValid = false;
+            if (input.Length == 4)
+            {
+                var year = 0;
+                int.TryParse(input, out year);
+                if (year >= 1920 && year <= 2002)
+                {
+                    isValid = true;
+                }
+            }
+            return isValid;
+        }
+
+        private static bool ValidateIyr(string input)
+        {
+            //four digits; at least 2010 and at most 2020.
+            var isValid = false;
+            if (input.Length == 4)
+            {
+                var year = 0;
+                int.TryParse(input, out year);
+                if (year >= 2010 && year <= 2020)
+                {
+                    isValid = true;
+                }
+            }
+            return isValid;
+        }
+
+        private static bool ValidateEyr(string input)
+        {
+            //four digits; at least 2020 and at most 2030.
+            var isValid = false;
+            if (input.Length == 4)
+            {
+                var year = 0;
+                int.TryParse(input, out year);
+                if (year >= 2020 && year <= 2030)
+                {
+                    isValid = true;
+                }
+            }
+            return isValid;
+        }
+
+        private static bool ValidateHgt(string input)
+        {
+            //a number followed by either cm or in:
+            //If cm, the number must be at least 150 and at most 193.
+            //If in, the number must be at least 59 and at most 76.
+            var height = 0;
+            int.TryParse(input.Substring(0, input.Length - 2), out height);
+            var isValid = (input.EndsWith("cm") && height >= 150 && height <= 193) || (input.EndsWith("in") && height >= 59 && height <= 76) ? true : false;
+            return isValid;
+        }
+
+        private static bool ValidateHcl(string input)
+        {
+            //a # followed by exactly six characters 0-9 or a-f.
+            var isValid = false;
+            try
+            {
+                var c = ColorTranslator.FromHtml(input);
+                isValid = true;
+            }
+            catch { }
+            
+            return isValid;
+        }
+
+        private static bool ValidateEcl(string input)
+        {
+            //exactly one of: amb blu brn gry grn hzl oth.
+            var validEcl = new string[] { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
+            var isValid = (validEcl.Contains(input)) ? true : false;
+            return isValid;
+        }
+
+        private static bool ValidatePid(string input)
+        {
+            //a nine-digit number, including leading zeroes.
+            var isValid = false;
+            if (input.Length == 9)
+            {
+                var pid = -1;
+                int.TryParse(input, out pid);
+                if (pid > -1)
+                {
+                    isValid = true;
+                }
+            }
+            return isValid;
         }
     }
 }
