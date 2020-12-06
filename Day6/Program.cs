928295740e284d4752d69688626c20374b3837da
Rob Frankel <rob@robFX.com>
2020-12-06 11:03:51 -0500

    Day 6: Got really cocky about Part 1...

---
+++ Day6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = @"C:\Users\nukem\source\repos\AdventOfCode\Day6\input.txt";
+            var input = System.IO.File.ReadAllText(file).Split("\n\n");
+
+            Console.WriteLine($"Answers: {CountAnswers(input)}");
+        }
+
+        private static int CountAnswers(string[] input)
+        {
+            int count = 0;
+            foreach (var item in input)
+            {
+                var answers = item.Replace("\n", "").Trim().ToCharArray();
+                count += answers.Distinct().Count();
+            }
+            return count;
+        }
+    }
+}
