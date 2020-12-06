82cac343b586d886548267f1f5f40e1ec958d7b2
Rob Frankel <rob@robFX.com>
2020-12-06 13:20:39 -0500

    Holy shit, it worked!

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace Day6
@@ -11,18 +10,37 @@ namespace Day6
             var file = @"C:\Users\nukem\source\repos\AdventOfCode\Day6\input.txt";
             var input = System.IO.File.ReadAllText(file).Split("\n\n");
 
-            Console.WriteLine($"Answers: {CountAnswers(input)}");
+            Console.WriteLine($"Unique Answers: {CountUniqueAnswers(input)}");
+            Console.WriteLine($"All Answered: {CountAllAnswered(input)}");
         }
 
-        private static int CountAnswers(string[] input)
+        private static object CountAllAnswered(string[] input)
         {
             int count = 0;
-            foreach (var item in input)
+            foreach (var group in input)
             {
-                var answers = item.Replace("\n", "").Trim().ToCharArray();
+                var answers = group.Split("\n", StringSplitOptions.RemoveEmptyEntries).ToList();
+                var first = answers[0].ToCharArray().ToList();
+                foreach (var item in answers)
+                {
+                    first = first.Intersect(item.ToCharArray().ToList()).ToList();
+                }
+                count += first.Count();
+            }
+            return count;
+        }
+
+        private static int CountUniqueAnswers(string[] input)
+        {
+            int count = 0;
+            foreach (var group in input)
+            {
+                var answers = group.Replace("\n", "").Trim().ToCharArray();
                 count += answers.Distinct().Count();
             }
             return count;
         }
+
+
     }
 }
