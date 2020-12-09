826d23532bb681216a4fcb12c45299a5aa033785
Rob Frankel <rob@robFX.com>
2020-12-09 12:42:56 -0500

    Day 8: Part 1

---
+++ Day8/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = @"C:\Users\nukem\source\repos\AdventOfCode\Day8\input.txt";
+            var input = System.IO.File.ReadAllLines(file);
+
+            RunInstructions(input);
+        }
+
+        private static void RunInstructions(string[] input)
+        {
+            var index = 0;
+            var accumulator = 0;
+            var instructions = new List<int>();
+            var doLoop = true;
+            while (doLoop)
+            {
+                if (instructions.Contains(index))
+                {
+                    doLoop = false;
+                }
+                else
+                {
+                    instructions.Add(index);
+                    var instruct = input[index].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    switch (instruct[0])
+                    {
+                        case "acc":
+                            accumulator += int.Parse(instruct[1]);
+                            index += 1;
+                            break;
+                        case "jmp":
+                            index += int.Parse(instruct[1]);
+                            break;
+                        case "nop":
+                            index += 1;
+                            break;
+                    }
+                }
+            }
+            Console.WriteLine($"Accumulator: {accumulator}");
+        }
+    }
+}
